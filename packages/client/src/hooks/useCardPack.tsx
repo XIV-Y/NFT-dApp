9f1b0f0b1e3594ab27ffa2aa3bce7c62d1d0e088
XIV-Y <yutodazepa.g@gmail.com>
2024-02-03 16:27:08 +0900

    add #6 GLSLでCardPackを実装

--- packages/client/src/hooks/useCardPack.tsx
+++ packages/client/src/hooks/useCardPack.tsx
@@ -1,26 +1,56 @@
 import { useFrame, Canvas, useLoader } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import { BoxGeometry, TextureLoader } from 'three';
+import { useRef } from 'react';
+import { TextureLoader } from 'three';
+import * as THREE from 'three';
+
+const SphereShaderMaterial = {
+  uniforms: {
+    u_time: { type: 'f', value: 0 },
+    u_texture: { type: 'f', value: null },
+  },
+  vertexShader: `
+    precision mediump float;
+    varying vec2 vUv;
+    void main() {
+        vec4 mvPosition = modelViewMatrix * vec4(position, 1.);
+        gl_Position = projectionMatrix * mvPosition;
+        vUv = uv;
+    }
+  `,
+  fragmentShader: `
+    varying vec2 vUv;
+
+    uniform float u_time;
+    uniform sampler2D u_texture;      
+
+    void main() {
+      vec2 uv = vUv;
+      
+      gl_FragColor = texture2D(u_texture, uv); 
+    }
+  `,
+};
 
 export const useCardPack = () => {
   const Box = (props: JSX.IntrinsicElements['mesh']) => {
     const ref = useRef<THREE.Mesh>(null!);
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    useFrame((state, delta) => (ref.current.rotation.y += 0.01));
+    // useFrame((state, delta) => (ref.current.rotation.y += 0.01));
 
     const colorMap = useLoader(TextureLoader, 'main.svg');
 
+    useFrame(({ clock }) => {
+      ref.current.rotation.y += 0.01;
+      (ref.current.material as any).uniforms.u_time.value =
+        clock.oldTime * 0.00005;
+    });
+
+    SphereShaderMaterial.uniforms.u_texture.value = colorMap;
+
     return (
-      <mesh
-        {...props}
-        ref={ref}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
-      >
+      <mesh {...props} ref={ref}>
         <boxGeometry args={[2.5, 4.2, 0.05]} />
         <meshStandardMaterial map={colorMap} />
+        <shaderMaterial attach="material" args={[SphereShaderMaterial]} />
       </mesh>
     );
   };
