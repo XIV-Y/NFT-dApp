c4e6976e1bbfc0d1269d793412632c796902472b
XIV-Y <yutodazepa.g@gmail.com>
2024-02-03 14:51:48 +0900

    remove #6 ant design を使うのをやめる

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,65 +1,17 @@
 'use client';
 
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Layout, theme } from 'antd';
 import React from 'react';
 
-const { Header, Content, Sider } = Layout;
-
-const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-
-const items2: MenuProps['items'] = [
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-].map((icon, index) => {
-  const key = String(index + 1);
-
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
-
 const Home: React.FC = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   return (
-    <Layout>
-      <Layout>
-        <Layout>
-          <Content
-            style={{
-              margin: 0,
-              minHeight: '100vh',
-              backgroundImage:
-                'linear-gradient(40deg, rgba(243, 221, 254, 1), rgba(255, 188, 212, 1) 20%, rgba(243, 221, 254, 0.75) 50%, rgba(237, 188, 245, 1) 84%, rgba(194, 109, 238, 1) 94%);',
-            }}
-          >
-            <img src="/main.svg" />
-          </Content>
-        </Layout>
-      </Layout>
-    </Layout>
+    <div
+      style={{
+        backgroundImage:
+          'linear-gradient(40deg, rgba(243, 221, 254, 1), rgba(255, 188, 212, 1) 20%, rgba(243, 221, 254, 0.75) 50%, rgba(237, 188, 245, 1) 84%, rgba(194, 109, 238, 1) 94%);',
+      }}
+    >
+      <img src="/main.svg" className="select-none" />
+    </div>
   );
 };
 
